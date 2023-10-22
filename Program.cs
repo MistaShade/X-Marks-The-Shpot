31a5a0b170b646eb0ad63ffb68340ec7f5faf1b1
Sam <114481646+MistaShade@users.noreply.github.com>
2023-10-22 17:44:45 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, x, y;
+            Console.Write("Enter the size of the X: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            for (y = 1; y <= num * 2 - 1; y++)
+            {
+                for (x = 1; x <= num * 2 - 1; x++)
+                {
+                    int distanceX = x > num ? x - num : num - x;
+                    int distanceY = y > num ? y - num : num - y;
+
+                    if (distanceX == distanceY)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write("_ ");
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+
+
+
+    }
+}
